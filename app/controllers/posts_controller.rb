cbccb3fbac0031e63f526f3e08296c4fac0c1117
shoiraTa <shoira.shakirovna@gmail.com>
2022-03-03 14:27:39 +0400

    Add create routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,28 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    @post = current_user.posts.new(post_params)
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+    @user = current_user
+    if post.save 
+      flash[:success] = 'Post was saved'
+      redirect_to "/users/#{post.author.id}/posts/"
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
   
   root 'users#index' 
-  resources :users, only:  [:index, :show]  do
-    resources :posts, only: [:index, :show]
+  resources :users, only:  [:index, :show,  ]  do
+    resources :posts, only: [:index, :show,:new, :create] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
   end
 end
